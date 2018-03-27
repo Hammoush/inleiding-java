68929411eb5af6741736f63703d531e63dc91e34
hammoush <mhammad1991h@gmail.com>
2018-03-27 14:39:09 +0200

    dit is de tweede opdarcht

---
+++ src/Opdracht2.java
@@ -0,0 +1,25 @@
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdracht2 extends Applet {
+
+    public void init() {
+
+        setBackground(new Color(69, 46, 221));
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(new Color(200, 216, 58));
+        g.drawString("Mohamad Hammoush", 50, 70);
+
+
+    }
+
+}
+
+
+
+
+
+
+
