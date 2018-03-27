960888607b9cab326b55066b1e5c1f7444e86aaf
hammoush <mhammad1991h@gmail.com>
2018-03-27 11:55:35 +0200

    Mijn eerste applet

--- src/h02/Opdracht1.java
+++ src/h02/Opdracht1.java
@@ -1,4 +1,19 @@
 package h02;
+import java.awt.*;
+import java.applet.*;
 
-public class Opdracht1 {
+
+public class Opdracht1 extends Applet {
+public void init(){
+ setBackground(new Color(255, 248, 220));
+
+}
+public void paint (Graphics g){
+ g.setColor(new Color (128, 0, 0));
+ g.drawString("Mijn eeste Applet!", 50, 70);
+
+
+}
+
+
 }
