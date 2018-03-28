263714de8458f8f33ddedc392df35a7bf0ce4938
hammoush <mhammad1991h@gmail.com>
2018-03-28 15:29:18 +0200

    Tekenen4-7

---
+++ src/h3/Opdracht47.java
@@ -0,0 +1,37 @@
+package h3;
+
+
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdracht47 extends Applet {
+   public void init(){
+
+setSize(400, 400);
+   }
+
+public void paint (Graphics g){
+       setBackground(Color.BLACK);
+       g.setColor(Color.darkGray);
+       g.fillRect(200, 200, 160, 160);
+       g.setColor(Color.green);
+       g.fillOval(220, 220, 30, 30);
+       g.setColor(Color.green);
+       g.fillOval(220, 300, 30, 30);
+    g.setColor(Color.green);
+    g.fillOval(310, 220, 30, 30);
+    g.setColor(Color.green);
+    g.fillOval(310, 300, 30, 30);
+
+
+
+
+
+
+}
+
+
+
+
+}
