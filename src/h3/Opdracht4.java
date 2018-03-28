aa4b549db57d9123830e70e192f14a0453ac700e
hammoush <mhammad1991h@gmail.com>
2018-03-28 12:15:56 +0200

    Tekenen-3

---
+++ src/h3/Opdracht4.java
@@ -0,0 +1,41 @@
+package h3;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdracht4 extends Applet {
+
+    public void init(){
+
+        setSize(500, 400);
+    }
+    public void paint(Graphics g){
+        setBackground(new Color(16, 209, 96));
+
+    g.setColor(new Color(20, 18, 17));
+    g.fillRect(150, 200, 3, 200);
+    g.setColor(new Color(234, 11, 11));
+    g.fillRect(150, 200, 250, 20);
+    g.setColor(new Color(255, 255, 255));
+    g.fillRect(150, 220, 250, 20);
+    g.setColor(new Color(16, 145, 201));
+    g.fillRect(150,240, 250, 20);
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
