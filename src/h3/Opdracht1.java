6de515e469e3ba95d73132a28203711af052244c
hammoush <mhammad1991h@gmail.com>
2018-03-28 10:57:49 +0200

    Mijn eerste applet

---
+++ src/h3/Opdracht1.java
@@ -0,0 +1,27 @@
+package h3;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdracht1 extends Applet {
+ public void init(){}
+
+
+ public void paint(Graphics g){
+     setBackground(new Color(244, 244, 230));
+     g.setColor(new Color(242, 242, 2));
+ g.fillArc(20, 20, 1000, 500, 0, 45);
+
+
+
+
+
+ }
+
+
+
+
+
+
+
+}
