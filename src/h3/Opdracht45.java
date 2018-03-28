6e85d0dbd9fe547d837efa540a82ab8c73225a73
hammoush <mhammad1991h@gmail.com>
2018-03-28 14:43:44 +0200

    Tekenen5

---
+++ src/h3/Opdracht45.java
@@ -0,0 +1,27 @@
+package h3;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdracht45 extends Applet {
+    public void init(){
+
+     setBackground(Color.blue);
+    }
+    public void paint(Graphics g){
+     g.setColor(Color.yellow);
+     g.fillOval(100, 100, 50, 100);
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+}
