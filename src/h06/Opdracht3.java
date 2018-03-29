e72e5305b80b208825fb6d90da414ad0816827e4
hammoush <mhammad1991h@gmail.com>
2018-03-29 14:06:10 +0200

    Opdracht6-3

---
+++ src/h06/Opdracht3.java
@@ -0,0 +1,17 @@
+package h06;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdracht3 extends Applet {
+    int nigatieve;
+    public void init(){
+    nigatieve = (2147483647+1);}
+    public  void  paint (Graphics g){
+        g.drawString("uitkomst  :  " +nigatieve , 20, 20);
+
+
+
+
+
+    }}
